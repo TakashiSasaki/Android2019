d066836ba0e29e4099d7fba75d67ad5239637b9d
Takashi SASAKI <takashi316@gmail.com>
2018-06-19 14:31:24 +0900

    p190

--- MenuSample/app/src/main/java/com/gmail/takashi316/menusample/ListViewActivity.java
+++ MenuSample/app/src/main/java/com/gmail/takashi316/menusample/ListViewActivity.java
@@ -3,6 +3,9 @@ package com.gmail.takashi316.menusample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -30,14 +33,21 @@ public class ListViewActivity extends Activity {
         setContentView(R.layout.activity_menu_list);
 
         //メンバ変数の初期化処理
-        _lvMenu = findViewById(R.id.lvMenu);
-        _menuList = createTeishokuList();
+        this._lvMenu = findViewById(R.id.lvMenu);
+        this._menuList = createTeishokuList();
 
         SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),
                 _menuList, R.layout.row, FROM, TO);
-        _lvMenu.setAdapter(adapter);
-        _lvMenu.setOnItemClickListener(new ListItemClickListener());
-    }
+        this._lvMenu.setAdapter(adapter);
+        this._lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }//onCreate
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options_menu_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }//onCreateOptionsMenu
 
     private List<Map<String, Object>> createTeishokuList() {
         List menuList = new ArrayList<>();
@@ -58,7 +68,26 @@ public class ListViewActivity extends Activity {
         return menuList;
     }//createTeishokuList
 
-    class ListItemClickListener implements ListView.OnItemClickListener{
+    private List<Map<String, Object>> createCurryList() {
+        List menuList = new ArrayList<>();
+
+        Map<String , Object> menu = new HashMap<>();
+
+        menu.put("name", "ビーフカレー");
+        menu.put("price", 520);
+        menu.put("desc", "特選スパイスを効かせた国産ビーフ１００％のカレーです。");
+        menuList.add(menu);
+
+        menu = new HashMap<>(); //これにより新しいMapオブジェクトを生成
+        menu.put("name", "ポークカレー");
+        menu.put("price", 420);
+        menu.put("desc", "特選スパイスを効かせた国産ポーク１００％のカレーです。");
+        menuList.add(menu);
+
+        return menuList;
+    }//createCurryList
+
+    private class ListItemClickListener implements ListView.OnItemClickListener{
 
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -73,4 +102,25 @@ public class ListViewActivity extends Activity {
             startActivity(intent);
         }
     }
-}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch(itemId){
+            case R.id.menuListOptionTeishoku:
+                this._menuList = createTeishokuList();
+                break;
+            case R.id.menuListOptionCurry:
+                this._menuList = createCurryList();
+                break;
+        }//switch
+
+        SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),
+                this._menuList, R.layout.row, this.FROM, this.TO);
+
+        this._lvMenu.setAdapter(adapter);
+
+        return super.onOptionsItemSelected(item);
+    }//onOptionsItemSelected
+}//ListViewActivity
