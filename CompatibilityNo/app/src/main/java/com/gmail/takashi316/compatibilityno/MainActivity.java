19c419a2c0855b72cf1cd319ab1d489b0c8d617c
Takashi SASAKI <takashi316@gmail.com>
2018-07-19 12:11:27 +0900

    CompatibilityYes and CompatibilityNo

---
+++ CompatibilityNo/app/src/main/java/com/gmail/takashi316/compatibilityno/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.takashi316.compatibilityno;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
