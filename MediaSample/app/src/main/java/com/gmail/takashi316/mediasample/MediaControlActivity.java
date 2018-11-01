1edf41e523dab50bd06ba44b1c379fd7ab327735
Takashi SASAKI <takashi316@gmail.com>
2018-11-02 08:24:03 +0900

    SurfaceView

--- MediaSample/app/src/main/java/com/gmail/takashi316/mediasample/MediaControlActivity.java
+++ MediaSample/app/src/main/java/com/gmail/takashi316/mediasample/MediaControlActivity.java
@@ -4,6 +4,8 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -35,14 +37,20 @@ public class MediaControlActivity extends AppCompatActivity {
                 new LoopSwitchChangedListener()
         );
 
+        //SurfaceViewのSurfaceHolderにコールバックを設定
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(new SurfaceHolderCallback());
+
         String mediaFileUriStr = "android.resource://"
                 + getPackageName() + "/"
-                + R.raw.kanikan_high;
+                + R.raw.kanikan_small;
 
-        // 以下のような文字列が組み立てられている↑
-        //String mediaFileUriStr2 = "android.resource://"
-        //        + "com.gmail.takashi316.mediasample/"
-        //        + Integer.parseInt("7f0b0000",16);
+        //以下のような文字列が組み立てられている↑
+        //mediaFileUriStr2は使わないけど例示のために残しておく
+        String mediaFileUriStr2 = "android.resource://"
+                + "com.gmail.takashi316.mediasample/"
+                + Integer.parseInt("7f0b0000",16);
 
         //URI文字列をURIオブジェクトに変換
         Uri mediaFileUri = Uri.parse(mediaFileUriStr);
@@ -55,6 +63,7 @@ public class MediaControlActivity extends AppCompatActivity {
         }catch (IOException e){
             e.printStackTrace();
         }//try
+
 
     }//onCreate
 
@@ -94,7 +103,7 @@ public class MediaControlActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(_player.isPlaying()) {
+        if(_player != null && _player.isPlaying()) {
             _player.start();
         } else {
             //特になにもすることなし
@@ -112,7 +121,7 @@ public class MediaControlActivity extends AppCompatActivity {
         if(!_player.isPlaying()) {
             _player.start();
         }
-    }
+    }//onForwardButtonClick
 
     private class LoopSwitchChangedListener
         implements CompoundButton.OnCheckedChangeListener {
@@ -122,5 +131,33 @@ public class MediaControlActivity extends AppCompatActivity {
             _player.setLooping(b);
         }//onCheckedChanged
     }//LoopSwitchChangedListener
+
+    // SurfaceView は準備ができたらSurfaceHolder.Callbackを呼び出す。
+    //コールバックを受け取るためのクラスを用意おこうと思った。
+    //ここでは無名プライベートクラスを定義してインスタンスを作る
+    //onCreate のsetContentViewですでにSurfaceViewは作られている。
+    //そのSurfaceViewに新たにSurfaceHolder.Callbackを設定するには？
+
+    private class SurfaceHolderCallback
+            implements SurfaceHolder.Callback {
+        @Override
+        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+            _player.setDisplay(surfaceHolder);
+        }//surfaceCreated
+
+        @Override
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+            //SurfaceView のサイズが変更されたときなどに呼び出される
+            //とりあえず今は何もしない。
+        }//surfaceChanged
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+            //アクティビティ自身のonDestroyでnullにされているかも知れない
+            if(_player != null) _player.release();
+            //アクティビティ自身のonDestroyで解放するので必須ではない
+            _player = null;
+        }//surfaceDestroyed
+    };//SurfaceHolderCallback
 
 }//MediaControlActivity
