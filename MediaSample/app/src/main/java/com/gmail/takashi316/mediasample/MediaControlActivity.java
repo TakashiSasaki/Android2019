8b43fb4eea2b87ab16187dd66f7f181af7664747
Takashi SASAKI <takashi316@gmail.com>
2018-11-01 14:27:07 +0900

    MediaSample

---
+++ MediaSample/app/src/main/java/com/gmail/takashi316/mediasample/MediaControlActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.takashi316.mediasample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MediaControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_control);
+    }
+}
