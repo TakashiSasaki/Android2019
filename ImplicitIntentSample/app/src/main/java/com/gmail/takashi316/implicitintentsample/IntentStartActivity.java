02d93bfb601ed1afb6621acacfe8a5a9ee947cb3
Takashi SASAKI <takashi316@gmail.com>
2018-11-13 14:52:58 +0900

    GPS

--- ImplicitIntentSample/app/src/main/java/com/gmail/takashi316/implicitintentsample/IntentStartActivity.java
+++ ImplicitIntentSample/app/src/main/java/com/gmail/takashi316/implicitintentsample/IntentStartActivity.java
@@ -1,7 +1,14 @@
 package com.gmail.takashi316.implicitintentsample;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +32,60 @@ public class IntentStartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intent_start);
         _tvLatitude = findViewById(R.id.tvLatitude);
         _tvLongitude = findViewById(R.id.tvLongitude);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions
+                    (IntentStartActivity.this, permissions, 1000);
+            return;
+        }//if
+        LocationManager locationManager
+                = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0, 0, new GPSLocationListener());
     }//onCreate
+
+    @Override
+    public void onRequestPermissionsResult
+            (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000 && grantResults[0]
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission
+                    (IntentStartActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates
+                        (LocationManager.GPS_PROVIDER,
+                                0,0,
+                                new GPSLocationListener());
+            }//if
+        }//if
+    }//onRequestPermissionsResult
+
+    class GPSLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            _longitude = location.getLongitude();
+            _latitude = location.getLatitude();
+            _tvLatitude.setText(""+_latitude);
+            _tvLongitude.setText(""+_longitude);
+        }//onLocationChanged
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+    }
 
     public void onMapSearchButtonClick(View view){
         EditText etSearchWord = findViewById(R.id.etSearchWord);
