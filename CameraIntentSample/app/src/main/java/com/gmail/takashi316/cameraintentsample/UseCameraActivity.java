038d8eab1aafd664a3bf4b7b9db5d795a0df2822
Takashi SASAKI <takashi316@gmail.com>
2018-11-20 22:38:30 +0900

    MediaStore.EXTRA_OUTPUT

--- CameraIntentSample/app/src/main/java/com/gmail/takashi316/cameraintentsample/UseCameraActivity.java
+++ CameraIntentSample/app/src/main/java/com/gmail/takashi316/cameraintentsample/UseCameraActivity.java
@@ -1,14 +1,26 @@
 package com.gmail.takashi316.cameraintentsample;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class UseCameraActivity extends AppCompatActivity {
+
+    private Uri _imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +31,56 @@ public class UseCameraActivity extends AppCompatActivity {
     @Override
     public void onActivityResult
             (int requestCode,int resultCode,Intent intent){
-        if(requestCode != 200) return;
-        if(resultCode != RESULT_OK) return;
-        Bitmap bitmap = intent.getParcelableExtra("data");
-        ImageView imageView = findViewById(R.id.ivCamera);
-        imageView.setImageBitmap(bitmap);
+        Log.d("onActivityResult", "requestCode = " + requestCode);
+        //教科書ではカメラアプリからビットマップを得る方法と
+        //MediaStoreコンテントプロバイダ経由で画像を得る方法を
+        //排他的に実装していたが、ここでは両方残し、requestCodeで区別する。
+        if(requestCode == 200 && resultCode == RESULT_OK) {
+            ImageView imageView = findViewById(R.id.ivCamera);
+            Bitmap bitmap = intent.getParcelableExtra("data");
+            imageView.setImageBitmap(bitmap);
+        }//if
+        if(requestCode == 300 && resultCode == RESULT_OK){
+            //startActivityForResultの際に指定したURIに保存されているはずなので、
+            //onActivityResultのintentから getParcelableExtra する必要は無い。
+            ImageView imageView = findViewById(R.id.ivCamera);
+            imageView.setImageURI(this._imageUri);
+        }//if
     }//onActivityResult
 
     public void onCameraImageClick(View view){
         Intent intent = new Intent
                 (MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 200);
-    }
-}
+    }//onCameraImageClick
+
+    //教科書ではMediaStoreコンテントプロバイダ経由で画像を取得するコードを
+    // onCameraImageClick に記述していたが、専用のボタンをレイアウトに配置し
+    //そのイベントハンドラに同コードを記述する。
+    public void onButtonClick(View view){
+        //onButtonClick
+        if(ActivityCompat.checkSelfPermission
+                (this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions, 2000);
+        }//if
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date now = new Date(System.currentTimeMillis());
+        String nowStr = dateFormat.format(now);
+        String fileName = "UseActivityPhoto_" + nowStr + ".jpg";
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, fileName);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+
+        ContentResolver contentResolver = this.getContentResolver();
+        this._imageUri = contentResolver.insert
+                (MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri);
+        startActivityForResult(intent, 300);
+    }//onButtonClick
+}//UseCameraActivity
