f44358b29e4f9e5b3c2d2f89fd3897b36cfc4253
Takashi SASAKI <takashi316@gmail.com>
2019-07-19 09:47:58 +0900

    begin to lecture chap14

---
+++ chap14-ImplicitIntentSample/app/src/main/java/com/example/implicitintentsample/IntentStartActivity.java
@@ -0,0 +1,13 @@
+package com.example.implicitintentsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_start);
+    }
+}
