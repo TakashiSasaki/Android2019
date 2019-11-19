29f9624a6e57175a8f8c45ef00efb760f53c5c98
Takashi SASAKI <takashi316@gmail.com>
2019-11-19 12:33:04 +0900

    android.useAndroidX=false

---
+++ 16-ToolbarSample/app/src/main/java/com/example/toolbarsample/ScrollArticleActivity.java
@@ -0,0 +1,25 @@
+package com.example.toolbarsample;
+
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ScrollArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_article);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //toolbar.setSubtitleTextColor(Color.LTGRAY)
+        //setSupportActionBar(toolbar);
+        //setActionBar(toolbar);
+    }
+}
