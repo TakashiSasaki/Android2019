f451ed13ab06039b43e08593efe0cfc200ddb47b
Takashi SASAKI <takashi316@gmail.com>
2019-11-21 15:32:03 +0900

    remove test case and corresponding dependencies

--- 16-CoordinatorLayoutSample/app/src/main/java/com/example/coordinatorlayoutsample/ScrollArticleActivity.java
+++ 16-CoordinatorLayoutSample/app/src/main/java/com/example/coordinatorlayoutsample/ScrollArticleActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class ScrollArticleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scroll_article);
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setLogo(R.mipmap.ic_launcher);
         toolbar.setTitleTextColor(Color.WHITE);
